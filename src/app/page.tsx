ca48f51aec98d633760a4e7c2c29ae9eeac3905a
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-05-02 20:25:57 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Herosection from "@/components/ui/Herosection";
+import FeaturedCourses from "@/components/ui/FeaturedCourses";
 export default function Home() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
     <Herosection/> 
+    <FeaturedCourses/>
    </main>
   );
 }
